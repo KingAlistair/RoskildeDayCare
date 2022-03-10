e48f51c7837708c09805d43059013f81ea7d2592
Dani <you@example.com>
2022-03-10 10:15:52 +0100

    -Database set up

--- src/main/DataBase/DDL.sql
+++ src/main/DataBase/DDL.sql
@@ -1,54 +1,50 @@
-CREATE DATABASE dayCare;
+#DROP DATABASE daycare;
+
+CREATE DATABASE IF NOT EXISTS daycare;
 
 CREATE TABLE employees(
 id int AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(300),
-phoneNumber VARCHAr (30),
+phone_number VARCHAr (30),
 address VARCHAR (300));
 
 CREATE TABLE children(
 id int AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(300),
-parentId INT,
 birthdate DATE);
 
 CREATE TABLE parents(
 id int AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(300),
-childrenId int,
-FOREIGN KEY (childrenId) REFERENCES children(id),
-phoneNumber VARCHAr (30),
+phone_number VARCHAR (30),
 address VARCHAR (300));
 
 CREATE TABLE parents_waiting_list(
 id int AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(300),
-childrenId int,
-phoneNumber VARCHAr (30),
+phone_number VARCHAR (30),
 address VARCHAR (300));
 
 CREATE TABLE children_waiting_list(
 id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(300),
-parentId INT,
-FOREIGN KEY (parentId) REFERENCES parents_waiting_list(id),
 birthdate DATE);
 
 CREATE TABLE daycare_groups(
 name VARCHAR(300),
-employeeId INT,
-childrenId INT,
-FOREIGN KEY (employeeId) REFERENCES employees(id),
-FOREIGN KEY (childrenId) REFERENCES children(id));
+employee_id INT,
+children_id INT,
+FOREIGN KEY (employee_id) REFERENCES employees(id),
+FOREIGN KEY (children_id) REFERENCES children(id));
 
 CREATE TABLE schedule(
-shiftName VARCHAR(300),
-employeeId INT,
+shift_name VARCHAR(300),
+employee_id INT,
 time VARCHAR(300),
-FOREIGN KEY (employeeId) REFERENCES employees(id));
+FOREIGN KEY (employee_id) REFERENCES employees(id));
 
 CREATE TABLE family(
-childrenId INT,
-parentId INT,
-FOREIGN KEY (childrenId) REFERENCES children(id),
-FOREIGN KEY (parentId) REFERENCES parents(id));
+children_id INT,
+parent_id INT,
+FOREIGN KEY (children_id) REFERENCES children(id),
+FOREIGN KEY (parent_id) REFERENCES parents(id));
--- src/main/DataBase/DML.sql
+++ src/main/DataBase/DML.sql
@@ -1,9 +1,29 @@
-SELECT * FROM children
-INNER JOIN family f on children.id = f.childrenId
-INNER JOIN parents p on f.parentId = p.id
-WHERE p.name = 'Margaret Solo';
+INSERT INTO children (name,birthdate)
+VALUES ('Junior Smith','2018.02.02'),
+       ('Lola Solo','2018.06.10'),
+       ('Lily Solo','2018.06.10');
 
+INSERT INTO parents (name,phone_number,address)
+VALUES ('John Smith', '+45 22 21 30 20', 'Nyhavn 15.'),
+('Jane Smith', '+45 21 21 30 40', 'Nyhavn 15.'),
+('Margaret Solo', '+45 20 55 31 02', 'Ensom gade 1.');
 
-SELECT * FROM children
-INNER JOIN `groups` g on children.id = g.childrenId
-WHERE g.name = 'Bunny group';
+INSERT INTO employees (name, phone_number,address)
+VALUES ('Sandra Madsen', '+45 01 00 00 00', 'Chefvej 42.'),
+('Bob Kind', '+45 01 00 00 00', 'Rosevej 5.');
+
+INSERT INTO family
+VALUES (1,1),(1,2),(2,3),(3,3);
+
+INSERT INTO daycare_groups
+VALUES ('Bunny Group', 2,1),
+('Bunny Group', 2,3),
+('Bunny Group', 2,3);
+
+#Query to check if works
+SELECT children.name, parents.name,employees.name FROM children
+INNER JOIN family ON children.id = family.children_id
+INNER JOIN parents on family.parent_id = parents.id
+INNER JOIN daycare_groups ON children.id = daycare_groups.children_id
+INNER JOIN employees ON daycare_groups.employee_id = employees.id
+WHERE daycare_groups.name = 'Bunny group';
