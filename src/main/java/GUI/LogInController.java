eaf07f58ee8d80a74ebf7601863cfa591ca8b82c
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-03-11 16:29:20 +0100

    I deleted the hello-view.fxml
    I added log-in.fxml
    
    I started connecting DB with login button

---
+++ src/main/java/GUI/LogInController.java
@@ -0,0 +1,44 @@
+package GUI;
+
+import DB_connection.DB_Utils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogInController {
+    @FXML
+    private Button lgnBtn;
+
+    @FXML
+    private PasswordField pswFld;
+
+    @FXML
+    private TextField usrNmFld;
+
+
+
+
+    public void initialize(URL url, ResourceBundle rb){
+
+        lgnBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            //here we are connecting our fields with our login event
+            public void handle(ActionEvent event) {
+                DB_Utils.login(event, usrNmFld.getText(), pswFld.getText());
+            }
+        });
+
+    }
+
+
+
+}
+
+
