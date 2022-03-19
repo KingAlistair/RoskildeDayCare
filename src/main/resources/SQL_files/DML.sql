313ffad14b6e46e2e210bcc92cb89c5c2ae7f7fc
dani <dani@email.com>
2022-03-19 13:34:57 +0100

    -Added the old SQL files, we can still use them to have the same databases.

---
+++ src/main/resources/SQL_files/DML.sql
@@ -0,0 +1,33 @@
+INSERT INTO children (name,birthdate)
+VALUES ('Junior Smith','2018.02.02'),
+       ('Lola Solo','2018.06.10'),
+       ('Lily Solo','2018.06.10');
+
+INSERT INTO parents (name,phone_number,address)
+VALUES ('John Smith', '+45 22 21 30 20', 'Nyhavn 15.'),
+('Jane Smith', '+45 21 21 30 40', 'Nyhavn 15.'),
+('Margaret Solo', '+45 20 55 31 02', 'Ensom gade 1.');
+
+INSERT INTO employees (name, phone_number,address)
+VALUES ('Sandra Madsen', '+45 01 00 00 00', 'Chefvej 42.'),
+('Bob Kind', '+45 01 00 00 00', 'Rosevej 5.');
+
+INSERT INTO family
+VALUES (1,1),(1,2),(2,3),(3,3);
+
+INSERT INTO daycare_groups
+VALUES ('Bunny Group', 2,1),
+('Bunny Group', 2,2),
+('Bunny Group', 2,3);
+
+#Query to check if works - (spoiler it does)
+SELECT children.name, parents.name, employees.name FROM children
+INNER JOIN family ON children.id = family.children_id
+INNER JOIN parents ON family.parent_id = parents.id
+INNER JOIN daycare_groups ON children.id = daycare_groups.children_id
+INNER JOIN employees ON daycare_groups.employee_id = employees.id
+WHERE daycare_groups.name = 'Bunny group';
+
+INSERT INTO users (employee_id,username, password)
+VALUES (1,'SandraAdmin', 'admin123'),
+       (2,'BobKind', 'kind123');
